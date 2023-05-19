0bba1ab1fe96c992c004f558ed56f067c5edb556
obinna <obinna.atuorah@gmail.com>
2023-05-19 10:51:59 +0100

    deleted comments on adminDashboard.js

--- Bank App/adminDashboard.js
+++ Bank App/adminDashboard.js
@@ -1,11 +1,6 @@
-// console.log(localStorage);
 for (let x in localStorage){
-    // console.log(x);
     let y = localStorage.getItem(x);
-    // console.log(y);
     let z = JSON.parse(y);
-    // console.log(z);
-    // console.log(z.userName);
     let data;
     data = `<tr>
    <td>${z.userName}</td>
